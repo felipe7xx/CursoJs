ac1d3577f8d8724cecf29989dbe8d7c31793654e
felipe7xx <lipealvesdasilevira@gmail.com>
2024-07-30 03:47:01 -0300

    terminei a aula

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}km/`)
+if(vel > 60){ // Condição simples!
+    console.log(`Você ultrapassou a velocidade permita, MULTADO!`)
+}
+console.log(`Dirija sempre com cinto de segurança!`)
